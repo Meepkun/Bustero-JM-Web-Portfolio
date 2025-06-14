be901ec883f8852965284cd6b926cfc3cc63435f
Meepkun <jmbustero0940@gmail.com>
2025-06-14 14:40:11 +0800

    update

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -12,7 +12,42 @@ function AboutSection () {
             <div class="container px-5">
                 <img src="https://media2.giphy.com/media/pOEbLRT4SwD35IELiQ/200.webp?cid=790b7611xpoqf5aykblf83mbiuc4647w0q0bpb59peu36ehs&ep=v1_gifs_search&rid=200.webp&ct=g" class="border-outline  img-fluid rounded-3 " alt="Example image" width="auto" height="200" loading="lazy"/>
             </div>
+            <ul></ul>
             </div>
+            <ul></ul>
+        <h1 class="text-center display-6 fw-bold text-body-emphasis lh-1 mb-3">Family Background</h1>
+            <div class="col-lg-6 mx-auto ">
+            <p class="lead mb-4">I come from a humble family of six. My parents work hard to support us, and I have three younger siblings. We’ve always believed in the power of education, faith, and kindness. My family has been my biggest motivation throughout my academic journey.</p>
+            </div>
+            <div class="overflow-hidden">
+            </div>
+            <h1 class="text-center display-6 fw-bold text-body-emphasis lh-1 mb-3">Challenges</h1>
+            <div class="col-lg-6 mx-auto ">
+            <p class="lead mb-4">
+                <li>Debugging a complex app alone.</li>
+                <li>Presenting during thesis defense with stage fright.</li>
+                </p>
+                <ul></ul>
+            </div>
+            <h1 class="text-center display-6 fw-bold text-body-emphasis lh-1 mb-3">Learnings</h1>
+            <div class="col-lg-6 mx-auto ">
+                <p class="lead mb-4">
+                <li>Asking for help is a strength, not a weakness.</li>
+                <li>Failure teaches more than success ever will.</li>
+                </p>
+            </div>
+            <ul></ul>
+        <h1 class="text-center display-6 fw-bold text-body-emphasis lh-1 mb-3">Future Plans in Life</h1>
+            <div class="col-lg-6 mx-auto ">
+            <p class="lead mb-4">As a Computer Science student who loves both technology and the arts, I plan to build creative and useful digital tools, apps, or websites. I want to work in a company that values design and innovation, and someday create my own projects that combine art and tech to help people and make a difference. My dream is to turn imagination into real, impactful technology.</p>
+            <ul></ul>
+            <p class="lead mb-4 fw-bold">To improve my portfolio, I plan to</p>
+            <li>Add more real-world projects, especially those that solve practical problems.</li>
+            <li>Enhance the design and layout to better reflect my creativity.</li>
+            <li>Regularly update my GitHub with personal and school projects.</li>
+            <ul></ul>
+            </div>
+            
         </div>
         </>
     );
